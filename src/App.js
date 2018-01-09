1d756215f322a8a1d7c62fed972dfd94a792b76f
Schmidt-Aaron <aschmidt808@gmail.com>
2018-01-09 15:56:37 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.gridX = 50;
+    this.gridY = 30;
+
+    this.state =  {
+      generations: 0,
+    }
+    //if adding functions follow this template
+    //this.functionName = this.functionName.bind(this);
+  }
+    //then add function here, or inside applicable component
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Conway's Game of Life</h1>
+        </header>
+        <Grid
+          rows={this.gridX}
+          cols={this.gridY} 
+        />
+        <h2> Generation: {this.state.generations}</h2>
+      </div>
+    );
+  }
+}
+
+class Grid extends Component {
+  // constructor(props) {
+  //   super(props);
+
+  //   this.state =  {
+  //     gridX,
+  //     gridY,
+  
+  //   }
+  
+  // }
+  
+  render() {
+    //const  { gridX, gridY } = this.props;   
+    
+    return (
+      <div className="grid">
+        insert grid here
+      </div>
+    )
+  }
+}
+
+
+export default App;
