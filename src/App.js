4ed0c5fc8c6e619e4a39a6d53947d140911c0e44
Schmidt-Aaron <aschmidt808@gmail.com>
2018-02-06 15:51:08 -0800

    moar components!

--- src/App.js
+++ src/App.js
@@ -6,22 +6,59 @@ const makeArray = (cols, rows, random) => {
   /** 
    * @random should a bolean
    * will fill the array with either 0 or 1  if set to true  
-  **/
+   **/
   let density = 0.8; // modifies the density of the alive cells for initial grid.
   let arr = new Array(cols);
   for (let i = 0; i < arr.length; i++) {
     arr[i] = new Array(rows);
     for (let j = 0; j < arr.length; j++ ){
       !random 
-        ? arr[i][j] = 0
-        : arr[i][j] = ( Math.round( Math.random() * density )); 
+      ? arr[i][j] = 0
+      : arr[i][j] = ( Math.round( Math.random() * density )); 
     }
   }
   return arr;
 }
 
+//calculates cell neighbors 
+const calculateNeighbors = (gameArr, x, y) => {
+  let sum = 0;
+  let cols = this.state.cols;
+  let rows = this.state.rows;
+  for (let i = -1; i < 2; i++) {
+    for(let j = -1; j < 2; j++) {
+      let col = (x + i + cols) % cols;
+      let row = (y + j + rows) % rows;
+      sum += gameArr[col][row]
+    }
+  }
+  sum -= gameArr[x][y]
+  return sum;
+}
+
+const createNextGenGrid = (gameArr) => {
+  let nextGameArr = gameArr;
+  for(let i = 0; i < gameArr.length; i++){
+    for(let j = 0; j < gameArr[i].length; j++){
+      let cell = gameArr[i][j];
+      let neighbors = calculateNeighbors(gameArr, i, j);
+
+      if(cell === 0 && neighbors === 3) {
+        nextGameArr[i][j] = 1;
+      } else if (cell === 1 && (neighbors < 2 || neighbors > 3)) {
+        nextGameArr[i][j] = 0;
+      } else {
+        nextGameArr[i][j] = cell;
+      }
+    }
+  }
+
+  return nextGameArr;
+}
+
+
 //main component
-class App extends Component {
+class GoL extends Component {
   constructor(props) {
     super(props);
 
@@ -69,7 +106,7 @@ class App extends Component {
     if(this.state.updated === false) {
       setTimeout(() => {
         this.setState( (prevState) => ({
-          gameArray: this.createNextGenGrid(prevState.gameArray),
+          gameArray: createNextGenGrid(prevState.gameArray),
           updated: true, 
           generation: prevState.generation += 1,
         })) 
@@ -77,41 +114,6 @@ class App extends Component {
     }
   }
 
-  //calculates cell neighbors 
-  calculateNeighbors(gameArr, x, y) {
-    let sum = 0;
-    let cols = this.state.cols;
-    let rows = this.state.rows;
-    for (let i = -1; i < 2; i++) {
-      for(let j = -1; j < 2; j++) {
-        let col = (x + i + cols) % cols;
-        let row = (y + j + rows) % rows;
-        sum += gameArr[col][row]
-      }
-    }
-    sum -= gameArr[x][y]
-    return sum;
-  }
- 
-  createNextGenGrid(gameArr) {
-    let nextGameArr = gameArr;
-    for(let i = 0; i < gameArr.length; i++){
-      for(let j = 0; j < gameArr[i].length; j++){
-        let cell = gameArr[i][j];
-        let neighbors = this.calculateNeighbors(gameArr, i, j);
-
-        if(cell === 0 && neighbors === 3) {
-          nextGameArr[i][j] = 1;
-        } else if (cell === 1 && (neighbors < 2 || neighbors > 3)) {
-          nextGameArr[i][j] = 0;
-        } else {
-          nextGameArr[i][j] = cell;
-        }
-      }
-    }
-
-    return nextGameArr;
-  }
 
   startGame(){
     this.setState( (prevState) => ({
@@ -136,15 +138,6 @@ class App extends Component {
 
   }
 
-  componentWillMount() {
-    //creates initial grid
-    if(this.state.gameArray.length === 0 ) {
-      this.setState( (prevState) => ({
-        gameArray: makeArray(this.state.cols, this.state.rows, false),
-      }))
-    }
-    
-  }
   
   componentDidUpdate() {
     if(this.state.updated === true ) {
@@ -153,42 +146,31 @@ class App extends Component {
       }))
     }
   }
-  
+
+  componentWillMount() {
+    //creates initial grid
+    if(this.state.gameArray.length === 0 ) {
+      this.setState( (prevState) => ({
+        gameArray: makeArray(this.state.cols, this.state.rows, false),
+      }))
+    }
+  }
+
   componentWillUnmount() {
 
   }
   
-  render() {
-     
+  render() { 
     const generation = this.state.generation;
 
     return (
-      <div className="App">
-        <div className="header">
-          <h1>Conway's GoL in React</h1>
-        </div>
+      <div className="container">
         <Grid 
           game={this.state.gameArray}
           running={this.state.isRunning}
         />  
-        <h2>Generation: {generation}</h2>
-        <div className="controls">
-          <button onClick={this.newRandomGrid} >
-            Random
-          </button>
-          <button onClick={this.newBlankGrid} >
-            Blank
-          </button>
-          <button onClick={this.incrementGeneration} >
-            Increment
-          </button>
-          <button onClick={this.startGame} >
-            Start
-          </button>
-          <button onClick={this.stopGame} >
-            Stop
-          </button>
-        </div>
+        <Counter generation={generation} />
+        <Controls />
       </div>
     );
   }
@@ -196,10 +178,20 @@ class App extends Component {
 
 // does this need state => change into function?
 class Grid extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      rows: 50,
+      cols: 50,
+      gameArray: []
+    }
+  }
 
   
+
   render() {
-    const game = this.props.game;
+    const game = this.props.gameArray;
     return (
       <div 
         className="grid"  
@@ -209,7 +201,7 @@ class Grid extends Component {
         e.map((el, j) =>
           <Box 
             alive={game[i][j]}
-            index={`${i}_${j}`}
+            key={`${i}_${j}`}
           />
         )    
       )
@@ -224,12 +216,51 @@ class Box extends Component{
     return (
       <div
         className={this.props.alive === 1 ? "box alive" : "box"}
-        key={this.props.index}
       />
 
     );
   }
 }
 
+const Header = () => (
+  <div className="header">
+    <h1>Conway's GoL in React</h1>
+  </div>
+)
+
+const Counter = (props) => (
+  <h2>Generation: {props.generation}</h2>
+)
+
+const Controls = (props) => (
+  <div className="controls">
+    <button onClick={this.newRandomGrid} >
+      Random
+    </button>
+    <button onClick={this.newBlankGrid} >
+      Blank
+    </button>
+    <button onClick={this.incrementGeneration} >
+      Increment
+    </button>
+    <button onClick={this.startGame} >
+      Start
+    </button>
+    <button onClick={this.stopGame} >
+      Stop
+    </button>
+  </div>
+)
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <Header />
+        <GoL />
+      </div>
+    );
+  }
+}
 
 export default App;
